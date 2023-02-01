54bd019ebfc65bb93db1e4fdad1ae85808165d40
VLavallee <62902233+VLavallee@users.noreply.github.com>
2023-02-01 14:25:42 -0700

    Add project files.

---
+++ MarsRover/Models/Rover.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRover.Models
+{
+    public class Rover
+    {
+        public int Id { get; set; }
+        public string? Name{ get; set; }
+        public int sPosX { get; set; }
+        public int sPosY { get; set; }
+        public char sDir { get; set; }
+        public string? Input { get; set; }
+
+        public int? fPosX { get; set; }
+        public int? fPosY { get; set; }
+        public char? fDir { get; set; }
+    }
+}
